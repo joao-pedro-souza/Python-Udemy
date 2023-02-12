88d8af34c519139fe679072c2db0c6ffe2ba9273
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-11 21:42:44 -0300

    Create aula013.py

---
+++ aula013.py
@@ -0,0 +1,12 @@
+nome = 'João Pedro'
+altura = 1.85
+peso = 75
+imc = ... # IMC = peso / (altura x altura)
+imc = peso / (altura * altura)
+
+linha = f'{nome} tem {altura:.2f} de altura, pesa {peso} e seu IMC é {imc:.2f}'
+
+print(linha)
+
+
+
