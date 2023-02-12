c56643714474c6f89a1796bcfcb45c79814d8e22
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-12 15:58:37 -0300

    Create ex033.py

---
+++ ex033.py
@@ -0,0 +1,8 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Luiz Otávio'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
