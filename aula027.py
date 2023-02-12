754f6200b74eb9383ebd2bdec6efb6a6b9ccd566
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-12 13:41:31 -0300

    Create aula027.py

---
+++ aula027.py
@@ -0,0 +1,11 @@
+"""
+Fatiamento de strings
+ 0123456789
+ Olá mundo
+-987654321
+Fatiamento [i:f:p [::]]
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1])
