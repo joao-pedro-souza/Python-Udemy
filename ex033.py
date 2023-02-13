a26955cc1689a56ef44b5e0f9ea2ad136ecdfed5
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-13 18:15:27 -0300

    Update ex033.py

--- ex033.py
+++ ex033.py
@@ -2,7 +2,8 @@
 https://docs.python.org/pt-br/3/library/stdtypes.html
 Imutáveis que vimos: str, int, float, bool
 """
-string = 'Luiz Otávio'
-outra_variavel = f'{string[:3]}ABC{string[4:]}'
-print(string)
-print(outra_variavel)
+string = 'luiz Otávio'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.capitalize())
