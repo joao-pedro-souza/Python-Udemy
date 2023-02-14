d1ce4cb021797a0e7c3b18b6d766b6b11a78e00e
JP <pcjoaopedro40@gmail.com>
2023-02-14 19:41:06 -0300

    Adicionei aula045.py

---
+++ aula045.py
@@ -0,0 +1,15 @@
+"""
+Iterável -> str, range, etc (__iter__)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+# for letra in texto
+texto = 'Luiz' #iterável
+iterador = iter(texto) # iterator
+
+while True:
+    try:
+        print(next(iterador))
+    except StopIteration:
+        break
