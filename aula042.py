19fffcd300a5f296e188778f88ea9a35ffdb4f6c
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-02-13 21:06:13 -0300

    Update aula042.py

--- aula042.py
+++ aula042.py
@@ -7,14 +7,14 @@ qtd_apareceu_mais_vezes = 0
 letra_apareceu_mais_vezes = ''
 while i < len(frase):
     letra_atual = frase[i]
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+    qtd_atual = frase.count(letra_atual)
 
     if letra_atual == ' ':
         i += 1
         continue
 
-    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+    if qtd_apareceu_mais_vezes < qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
         letra_apareceu_mais_vezes = letra_atual
 
     i += 1
