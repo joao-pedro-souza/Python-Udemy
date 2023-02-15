f0fffd270eadbe9de23c10a83ab80791b0810764
JP <pcjoaopedro40@gmail.com>
2023-02-14 22:00:43 -0300

    Atualizei o exercicio

--- aula047.py
+++ aula047.py
@@ -14,7 +14,35 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+import os
+
 palavra_secreta = 'perfume'
+tentativas = 0
+letras_acertadas = ''
 
 while True:
     letra_usuario = input('Digite uma letra: ')
+    tentativas += 1
+
+    if len(letra_usuario) > 1:
+        print('Digite apenas uma letra')
+        continue
+
+    if letra_usuario in palavra_secreta:
+        letras_acertadas += letra_usuario
+        
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+
+    print(f'Palavra formada: {palavra_formada}')
+
+    if palavra_formada == palavra_secreta:
+        print('VOCÊ GANHOU! PARABÉNS!')
+        print(f'A palavra era {palavra_secreta}')
+        print(f'Tentativas: {tentativas}')
+        letras_acertadas = ''
+        tentativas = 0
