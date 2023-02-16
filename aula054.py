ebdbf295b3b90ba9c994eaa80d215bbe83cbe4be
JP <pcjoaopedro40@gmail.com>
2023-02-16 19:18:53 -0300

    Atualizando aula054.py

--- aula054.py
+++ aula054.py
@@ -5,16 +5,21 @@ inserir, apagar e listar valores da sua lista
 Não permita que o programa quebre com
 erros de índices inexistentes na lista.
 """
+import os
+
 lista = []
+
 while True:
     print('Selecione uma opção')
     escolha_usuario = input('[i]nserir [a]pagar [l]istar: ')
     
     if escolha_usuario.lower() == 'i':
+        os.system('cls')
         item_lista = input('Valor: ')
         lista.append(item_lista)
 
-    if escolha_usuario.lower() == 'a':
+    elif escolha_usuario.lower() == 'a':
+        os.system('cls')
         valor = input('Valor: ')
         try:
             indice = int(valor)
@@ -22,9 +27,12 @@ while True:
         except:
             print('Esse valor não existe')
 
-    if escolha_usuario.lower() == 'l':
+    elif escolha_usuario.lower() == 'l':
+        os.system('cls')
         if len(lista) == 0:
             print('Lista vazia')
         else:
             for índice, nome in enumerate(lista):
                 print(índice, nome) 
+    else:
+        print('Por favor digite i, a ou l')
