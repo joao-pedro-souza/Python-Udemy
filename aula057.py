37acf4a28d2705cc37ec9a17ee5d4c0ed4739776
JP <pcjoaopedro40@gmail.com>
2023-02-17 16:03:01 -0300

    Adicionei aula057.py

---
+++ aula057.py
@@ -0,0 +1,15 @@
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    ['Maria', 'Helena'],
+
+    ['Elaine'],
+
+    ['Luiz', 'João', 'Eduarda']
+]
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
